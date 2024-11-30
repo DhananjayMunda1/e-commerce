20304299ecc4dbd7c71e720ff87631f94924a918
Dhananjay <dhruvasn2504@gmail.com>
2024-11-30 08:29:13 +0530

    Your commit message

--- script.js
+++ script.js
@@ -5,11 +5,34 @@ const nav=document.getElementsByClassName('nav')[0];
 // to check if the nav bar is showing in the screen or not
 // if any one click on the bar an event listner is there
 
+const close=document.getElementById('close');
+
+
 if(bar){
     bar.addEventListener('click',()=>{
-        nav.classList.add('active');
-        console.log("hello");
+        nav.classList.toggle('active');
+        // toggle was use earlier
     })
+    // This line adds an event listener to the entire document 
+    // that listens for any click events. 
+    // When a click occurs anywhere on the page, the function inside the event 
+    // listener will be executed.
+    document.addEventListener('click',(event)=>{
+        if(!nav.contains(event.target)&&event.target!==bar){
+            nav.classList.remove('active');
+        }
+    })
+}   
+
+if(close){
+    close.addEventListener('click',()=>{
+        nav.classList.remove('active');
+    })
+    // document.addEventListener('click',(event)=>{
+    //     if(!nav.contains(event.target)&&event.target!==bar){
+    //         nav.classList.remove('active');
+    //     }
+    // })
 }
 
 
